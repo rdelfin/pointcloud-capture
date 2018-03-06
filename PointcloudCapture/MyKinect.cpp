0bc7af81323ead9322791467c22f321054d36dd1
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-03-06 13:56:49 -0600

    Added private rgb and depth data fetchers

--- PointcloudCapture/MyKinect.cpp
+++ PointcloudCapture/MyKinect.cpp
@@ -10,21 +10,84 @@ MyKinect::MyKinect()
 }
 
 bool MyKinect::kinect_init() {
-	if (FAILED(GetDefaultKinectSensor(&sensor))) {
+	if (FAILED(GetDefaultKinectSensor(&this->sensor))) {
 		return false;
 	}
-	if (sensor) {
-		sensor->get_CoordinateMapper(&mapper);
+	if (this->sensor) {
+		this->sensor->get_CoordinateMapper(&this->mapper);
 
-		sensor->Open();
-		sensor->OpenMultiSourceFrameReader(
+		this->sensor->Open();
+		this->sensor->OpenMultiSourceFrameReader(
 			FrameSourceTypes::FrameSourceTypes_Depth | FrameSourceTypes::FrameSourceTypes_Color,
-			&reader);
-		return reader;
+			&this->reader);
+		return this->reader;
 	}
 	else {
 		return false;
 	}
+}
+
+void MyKinect::get_depth_data(IMultiSourceFrame* frame) {
+	IDepthFrame* depthframe;
+	IDepthFrameReference* frameref = NULL;
+	frame->get_DepthFrameReference(&frameref);
+	frameref->AcquireFrame(&depthframe);
+	if (frameref) frameref->Release();
+
+	if (!depthframe) return;
+
+	// Get data from frame
+	unsigned int sz;
+	unsigned short* buf;
+	depthframe->AccessUnderlyingBuffer(&sz, &buf);
+	
+	const int IMG_SIZE = POINTS_WIDTH * POINTS_HEIGHT;
+	this->depth_points.resize(IMG_SIZE);
+	this->color_points.resize(IMG_SIZE);
+
+	// Write vertex coordinates
+	mapper->MapDepthFrameToCameraSpace(IMG_SIZE, buf, IMG_SIZE, &this->depth_points[0]);
+
+	// Fill in depth2rgb map
+	mapper->MapDepthFrameToColorSpace(IMG_SIZE, buf, IMG_SIZE, &this->color_points[0]);
+	if (depthframe) depthframe->Release();
+}
+
+
+void MyKinect::get_rgb_data(IMultiSourceFrame* frame) {
+	IColorFrame* colorframe;
+	IColorFrameReference* frameref = NULL;
+	frame->get_ColorFrameReference(&frameref);
+	frameref->AcquireFrame(&colorframe);
+	if (frameref) frameref->Release();
+
+	if (!colorframe) return;
+
+	const int IMG_SIZE = COLOR_WIDTH * COLOR_HEIGHT;
+	this->image_data.resize(IMG_SIZE * 4);
+	this->image_colors.resize(IMG_SIZE);
+
+	// Get data from frame
+	colorframe->CopyConvertedFrameDataToArray(IMG_SIZE * 4, &this->image_data[0], ColorImageFormat_Rgba);
+	
+	// Write color array for vertices
+	for (int i = 0; i < IMG_SIZE; i++) {
+		ColorSpacePoint p = this->color_points[i];
+		// Check if color pixel coordinates are in bounds
+		// Intentionally skipping the alpha channel
+		if (p.X < 0 || p.Y < 0 || p.X > COLOR_WIDTH || p.Y > COLOR_HEIGHT) {
+			this->image_colors[i] = Color();
+		}
+		else {
+			int idx = (int)p.X + COLOR_WIDTH * (int)p.Y;
+			this->image_colors[i] = Color(
+				this->image_data[4 * idx + 0] / 255.,
+				this->image_data[4 * idx + 1] / 255.,
+				this->image_data[4 * idx + 2] / 255.);
+		}
+	}
+
+	if (colorframe) colorframe->Release();
 }
 
 
--- PointcloudCapture/MyKinect.h
+++ PointcloudCapture/MyKinect.h
@@ -1,4 +1,17 @@
 #pragma once
+
+const int POINTS_WIDTH = 512;
+const int POINTS_HEIGHT = 424;
+const int COLOR_WIDTH = 1920;
+const int COLOR_HEIGHT = 1080;
+
+class Color {
+public:
+	Color() : r(0), g(0), b(0) {};
+	Color(float r, float g, float b) : r(r), g(g), b(b) { }
+	float r, g, b;
+};
+
 class MyKinect
 {
 public:
@@ -7,6 +20,13 @@ public:
 
 private:
 	bool kinect_init();
+	void get_depth_data(IMultiSourceFrame* frame);
+	void get_rgb_data(IMultiSourceFrame* frame);
+
+	std::vector<CameraSpacePoint> depth_points;
+	std::vector<ColorSpacePoint>& color_points;
+	std::vector<uint8_t>& image_data;
+	std::vector<Color>& image_colors;
 
 	IKinectSensor * sensor;             // Kinect sensor
 	IMultiSourceFrameReader* reader;   // Kinect data source
